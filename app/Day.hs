0d02326cc7734b00efa02798a76afd41d3bc4dc4
Ioannis Katsaros <katsarosia@gmail.com>
2024-10-12 15:23:07 +0200

    Mudding it through

---
+++ app/Day.hs
@@ -0,0 +1,14 @@
+module Day where
+
+import Data.Aeson (FromJSON,ToJSON)
+import GHC.Generics (Generic)
+
+data Day 
+  = Monday 
+  | Tuesday
+  | Wednesday  
+  | Thursday 
+  | Friday 
+  | Saturday 
+  | Sunday
+  deriving (Show, Eq, Generic, FromJSON , ToJSON)
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,50 @@
+ {-# LANGUAGE OverloadedRecordDot #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Servant
+import Data.Text
+import Day
+import Network.Wai.Handler.Warp (run)
+import Data.Aeson
 
+
+main :: IO ()
+main = do
+  putStrLn "Running on 8000"
+  run 8000 app
+  putStrLn "Stopping the server..."
+
+app :: Application
+app = serve proxy server1
+
+proxy :: Proxy MyServantAPI
+proxy = Proxy
+
+server1 :: Server MyServantAPI   
+server1 = pure availabilities   
+
+type MyServantAPI = "api" :> Get '[JSON] [Availability] 
+-- http GET localhost:8000/api
+-- 
+
+data Availability = Availability 
+  { day :: Day
+  , hours :: Text
+  }
+
+instance ToJSON Availability where
+  toJSON availability = 
+    object 
+      [ "day" .= toJSON availability.day
+      , "hours" .= toJSON availability.hours
+      ]
+
+availabilities :: [Availability]
+availabilities =
+  [ Availability Monday "8am-12pm 14pm-18pm" 
+  , Availability Tuesday "8am-12pm 14pm-18pm" 
+  , Availability Wednesday "8am-12pm 14pm-18pm" 
+  , Availability Thursday "8am-12pm 14pm-18pm" 
+  , Availability Friday "8am-12pm 14pm-18pm"   
+  ]
